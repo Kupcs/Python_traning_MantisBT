a75136d17562cbb9d6108dbf608426a94009731d
Kupcs <kirinspb@gmail.com>
2022-04-27 08:37:52 +0300

    забыл добавить файл conftest

---
+++ conftest.py
@@ -0,0 +1,76 @@
+import pytest
+from fixture.application import Application
+import json
+import os
+# import importlib
+# import jsonpickle
+
+fixture = None
+target = None
+
+def load_config(file):
+    global target
+    #загрузка конфугурации
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
+@pytest.fixture()
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    # загрузка конфугурации
+    web_config = load_config(request.config.getoption("--target"))["web"]
+    web_admin = load_config(request.config.getoption("--target"))["webadmin"]
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, baseurl=web_config["baseUrl"])
+    #Нужен login или нет
+    fixture.session.ensure_login(username=web_admin["username"], password=web_admin["password"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        #Убедиться, что мы вышли из системы
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+
+# def pytest_generate_tests(metafunc):
+#     for fixture in metafunc.fixturenames:
+#         if fixture.startswith("data_"):
+#             testdata = load_from_module(fixture[5:])
+#             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+#         elif fixture.startswith("json_"):
+#             testdata = load_from_json(fixture[5:])
+#             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+# #
+# # #
+# def load_from_module(module):
+#     return importlib.import_module("data.%s" % module).test_data
+
+
+# def load_from_json(file):
+#     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+#         return jsonpickle.decode(f.read())
+
+
+# @pytest.fixture
+# def check_ui(request):
+#     return request.config.getoption("--check_ui")
+#
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--target", action="store", default="target.json")
+    # parser.addoption("--check_ui", action="store_true")
+
+
+
+
