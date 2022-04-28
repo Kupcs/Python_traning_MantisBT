dbeaa43cd44cb96a1b2f7ee9bac48bff604ddd1a
Kupcs <kirinspb@gmail.com>
2022-04-28 10:50:02 +0300

    реализован класс SoapHelper

---
+++ fixture/soap.py
@@ -0,0 +1,31 @@
+from suds.client import Client
+from suds import WebFault
+from model.project import Project
+
+
+class SoapHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+# проверяем, может ли юзер залогиниться
+    def can_login(self, username, password):
+        soap_config = self.app.config["soap"]
+        client = Client(soap_config["host"])
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
+
+
+    def get_projects(self):
+        project_list = []
+        soap_config = self.app.config["soap"]
+        client = Client(soap_config["host"])
+        projects = client.service.mc_projects_get_user_accessible(self.app.config['webadmin']['username'],
+                                                                  self.app.config['webadmin']['password'])
+        for row in projects:
+            project_list.append(Project(id=row.id, name=row.name, status=row.status.name, view_state=row.view_state.name,
+                                        description=row.description))
+        return project_list
